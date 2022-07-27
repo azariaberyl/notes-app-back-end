e1a9d1ef4e313f6526102ba421d596e6d0385582
azariaberyl <beryl.naga@gmail.com>
2022-07-27 20:38:08 +0700

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,15 @@
+const notes = [
+  {
+    'id': 'notes-V1StGXR8_Z5jdHi6B',
+    'title': 'Catatan 1',
+    'createdAt': '2020-12-23T23:00:09.686Z',
+    'updatedAt': '2020-12-23T23:00:09.686Z',
+    'tags': [
+      'Tag 1',
+      'Tag 2',
+    ],
+    'body': 'Isi dari catatan 1',
+  },
+];
+
+module.exports = notes;
